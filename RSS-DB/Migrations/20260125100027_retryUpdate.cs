78e9b141db029cfe021a5f0cecd635edaba39825
Zach Smith <zachsmith852@gmail.com>
2026-01-25 02:39:13 -0800

    retry file adds

---
+++ RSS-DB/Migrations/20260125100027_retryUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSS_DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class retryUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GameType",
+                table: "AvailableGames",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GameType",
+                table: "AvailableGames");
+        }
+    }
+}
